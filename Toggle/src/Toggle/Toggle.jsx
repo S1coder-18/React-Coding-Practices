ac7ea877d3c864e98795d8bc065eb6b3d269f465
sachin Gupta <sachinguptasg7633780@gmail.com>
2025-08-05 13:17:26 +0530

    Toggle functionality with image

--- Toggle/src/Toggle/Toggle.jsx
+++ Toggle/src/Toggle/Toggle.jsx
@@ -1,1 +1,26 @@
+import { useState } from "react"
+import './Toggle.css';
+import sachin_image from '../assets/Sachin.jpeg'
+export const Toggle = () =>{
+    const [toggle, setToggle] = useState(false);
 
+    const handleToggle = () =>{
+        setToggle(!toggle);
+    }
+
+    return(
+        <>
+        <div className="toggle">
+            
+            <button onClick={handleToggle}>{toggle ? 'Hide Text' : 'Show Text'}</button>
+            {
+                toggle ? (
+                    <div className="text">HI, Sachin You are smart, You cant achieve anything!.</div>
+                ) : ( <img src={sachin_image} alt="Image" />
+
+                )
+            }
+        </div>
+        </>
+    )
+}
